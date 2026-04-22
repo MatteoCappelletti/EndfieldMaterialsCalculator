2d3a53620fc13702caeba4ca393c233d3bf66011
Matteo Cappelletti <matteo.cappelletti@itsincom.it>
2026-04-22 10:55:15 +0200

    dati

--- src/models/ascension.py
+++ src/models/ascension.py
@@ -14,3 +14,18 @@ class Ascension:
         self.protoset = protoset
         self.flower = flower
         self.gold_item = gold_item
+
+ASCENSIONS = [
+    (20, Ascension(1600, 8, 0, 3, 0)),       # 20+
+    (40, Ascension(6500, 25, 0, 5, 0)),      # 40+
+    (60, Ascension(18000, 0, 24, 5, 0)),     # 60+
+    (80, Ascension(100000, 0, 36, 8, 20))    # 80+
+]
+
+# ascensione
+# - 4 (20+, 40+, 60+, 80+)
+#   - gold
+#   - protodisk
+#   - protoset
+#   - flower (variable)
+#   - 5* item (variable)
--- src/models/skill.py
+++ src/models/skill.py
@@ -17,3 +17,26 @@ class Skill:
         self.flower = flower
         self.perseverance_mask = perseverance_mask
         self.gold_item = gold_item
+
+SKILLS = [
+    Skill(1000, 6, 0, 1, 0, 0),
+    Skill(2700, 12, 0, 2, 0, 0),
+    Skill(3200, 16, 0, 1, 0, 0),
+    Skill(4200, 21, 0, 1, 0, 0),
+    Skill(5400, 27, 0, 2, 0, 0),
+    Skill(8200, 0, 6, 1, 0, 0),
+    Skill(10500, 0, 8, 1, 0, 0),
+    Skill(18000, 0, 15, 2, 0, 0),
+    Skill(24000, 0, 15, 3, 1, 6),
+    Skill(30000, 0, 24, 6, 2, 16),
+    Skill(65000, 0, 50, 12, 3, 36)
+]
+
+# skill
+# - 12 (1, 2, 3, 4, ... 11, 12)
+#   - gold
+#   - protoprism
+#   - protohedron
+#   - flower (variable)
+#   - perseverance mark (variable)
+#   - 5* item (variable)
